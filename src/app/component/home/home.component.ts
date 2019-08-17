5934d38b750657bdb22b651ea1407c3de514f756
ajani eniola <eniolsolomon@gmail.com>
2019-08-17 16:29:49 +0100

    feat dashboard

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild} from "@angular/core";
+import {ClrWizard} from "@clr/angular";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  //@ViewChild("wizard") wizard: ClrWizard;
+  skipStepTwo: boolean = true;
+  _open: boolean = false;
 
+  toggleStepTwo() {
+      this.skipStepTwo = !this.skipStepTwo;
+  }
+
+  open() {
+      this._open = true;
+     console.log(this._open);
+  }
   constructor() { }
 
   ngOnInit() {
