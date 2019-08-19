3c39af60a0d180ebb0d30418a5598726014ab993
ajani eniola <eniolsolomon@gmail.com>
2019-08-19 22:02:25 +0100

    feat routing complete

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -6,13 +6,15 @@ import { DashboardComponent } from 'src/app/component/dashboard/dashboard.compon
 import { HomeComponent } from 'src/app/component/home/home.component';
 import { SharedModule } from 'src/app/core/shared/shared.module';
 import { VesselComponent } from 'src/app/component/vessel/vessel.component';
+import { InfoComponent } from '../component/info/info.component';
+import { HistoryComponent } from '../component/history/history.component';
 
 @NgModule({
-  declarations: [DashboardComponent,HomeComponent,VesselComponent ],
+  declarations: [DashboardComponent,HomeComponent,VesselComponent,InfoComponent,HistoryComponent ],
   imports: [
     CommonModule,
     MainRoutingModule,
-    SharedModule
+    SharedModule,
   ]
 })
 export class MainModule { }
