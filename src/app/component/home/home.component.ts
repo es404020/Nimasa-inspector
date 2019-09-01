3c9d2a645d3463b79a1360bfa57c45bf9f0fe231
ajani eniola <eniolsolomon@gmail.com>
2019-09-01 18:24:27 +0100

    reactive form to add vessel complete

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {  ServiceService} from '../../core/service/service.service'
-
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,6 +11,8 @@ export class HomeComponent implements OnInit {
   skipStepTwo: boolean = true;
   _open: boolean = false;
   _country;
+  _vessels:FormGroup;
+  _isLoading:boolean;
 
   toggleStepTwo() {
       this.skipStepTwo = !this.skipStepTwo;
@@ -20,10 +22,54 @@ export class HomeComponent implements OnInit {
       this._open = true;
      console.log(this._open);
   }
-  constructor(private _service:ServiceService) { }
+  constructor(private _service:ServiceService,private _fb: FormBuilder) { }
 
   ngOnInit() {
+    this._isLoading = false;
     this._country=this._service.getAllFlag();
+    this._vessels = this._fb.group({
+   NameOfVessel :['', [
+        Validators.required
+      ]],
+    IMO:['', [
+        Validators.required
+      ]],
+      DOB:['', [
+        Validators.required
+      ]],
+      GT:['', [
+        Validators.required
+      ]],
+      FLAG:['', [
+        Validators.required
+      ]],
+      OfficalNo:['', [
+        Validators.required
+      ]],
+      RegDate:['', [
+        Validators.required
+      ]],
+      NigeriaOperator:['', [
+        Validators.required
+      ]],
+      CargoMeasurent:['', [
+        Validators.required
+      ]],
+       CargoType:['', [
+        Validators.required
+      ]],
+     ContactNumber:['', [
+        Validators.required
+      ]],
+
+    });
+
+  }
+
+  AddVessel(){
+    console.log(this._vessels.value);
+
+    this._vessels.reset();
   }
 
 }
