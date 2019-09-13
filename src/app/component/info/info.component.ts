01f35c7be31fdf40f38e8c28e74f5a234e0036c2
ajani eniola <eniolsolomon@gmail.com>
2019-09-13 16:20:39 +0100

    vessels page details

--- src/app/component/info/info.component.ts
+++ src/app/component/info/info.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit,ViewChild } from '@angular/core';
 import { Location } from '@angular/common';
 import {Router} from '@angular/router';
 import { from } from 'rxjs';
+import { SharedService } from 'src/app/core/shared/shared.service';
+
+
+
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
@@ -9,13 +13,29 @@ import { from } from 'rxjs';
 })
 export class InfoComponent  {
   _open: boolean = false;
+  _vessel:any;
+  _flagImage:any;
+  _flagName:any;
 
  
-  constructor(private location: Location,private _router:Router) { 
+  constructor(private location: Location,private _router:Router,private share:SharedService) { 
     
   }
 
   ngOnInit() {
+    this.share.getVesselInfo().
+    subscribe((res)=>{
+      console.log(res);
+      if(res===null){
+        this._vessel = null
+        this._router.navigate(['home/vessels'])
+      }else{
+        this._vessel = res;
+        this._flagName =res.FLAG.split(",")[0] ;
+        this._flagImage=res.FLAG.split(",")[1];
+      }
+     
+    })
   
   }
 
